313dc3372c1d60d4f92ef8f2a98b15e769e23609
Romain Braun <braun.rom@gmail.com>
2016-02-04 19:50:31 +0000

    initial setup

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('shouldIgo')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('shouldIgo', ['ngAnimate', 'ngMessages', 'ui.router', 'toastr']);
+
+})();
